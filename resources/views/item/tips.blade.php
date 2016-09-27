53e9a63e9492854e630711c7a40edc3e74564732
Robinson <cbb12345678@163.com>
2016-09-27 18:04:37 +0800

    tips

---
+++ resources/views/item/tips.blade.php
@@ -0,0 +1,19 @@
+<div class="col-md-12 " style="border-bottom:  2px solid #E4E3E5;
+	{{-- margin-top: 35px; --}}
+	margin-bottom: 40px;
+">
+	<div class="col-md-5 col-md-offset-4" style="
+margin-top: 35px;
+">
+		<h1 >欢迎大家选择部门</h1>
+
+	</div>
+<div class="col-md-7 col-md-offset-3" style="
+margin-bottom: 35px;
+">
+<p>
+	因为俱乐部处于发展初期，所以希望大家都能贡献出自己的一份力量。
+</p>
+</div>
+
+</div>
