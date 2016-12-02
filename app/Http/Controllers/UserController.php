260b850f5055c8b97c405aeada05af839bf608d2
Robinson28years <1783555833@qq.com>
2016-12-02 19:32:24 +0800

    微信

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,6 +29,7 @@ class UserController extends Controller
 
     public function user($openId)
     {
+        dd($openId);
         $user = $this->wechat->user->get($openId);
         return $user;
     }
