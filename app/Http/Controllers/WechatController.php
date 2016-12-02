7d41bf6756a0a5eb3eb91ae9bfc93beaf6c03995
Robinson28years <1783555833@qq.com>
2016-12-02 18:59:16 +0800

    微信

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -20,9 +20,36 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
-        });
+        $userApi = $wechat->user;
+        $wechat->server->setMessageHandler(function ($message) use ($userApi) {
+            switch ($message->MsgType) {
+                case 'event':
+                    # 事件消息...
+                    break;
+                case 'text':
+                    # 文字消息...
+                    return '你好'.$userApi->get($message->FromUserName)->nickname;
+                    break;
+                case 'image':
+                    # 图片消息...
+                    break;
+                case 'voice':
+                    # 语音消息...
+                    break;
+                case 'video':
+                    # 视频消息...
+                    break;
+                case 'location':
+                    # 坐标消息...
+                    break;
+                case 'link':
+                    # 链接消息...
+                    break;
+                // ... 其它消息
+                default:
+                    # code...
+                    break;
+            }
 
         Log::info('return response.');
 
