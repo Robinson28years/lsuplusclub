2501cca8836115512a2831967189da6680d5136c
Robinson28years <1783555833@qq.com>
2016-12-03 14:35:04 +0800

    帖子获取内容变更

--- app/Api/Transformers/CommentsTransformers.php
+++ app/Api/Transformers/CommentsTransformers.php
@@ -8,21 +8,27 @@
 
 namespace App\Api\Transformers;
 
+use App\Comment;
 use App\Discuss;
 use League\Fractal\TransformerAbstract;
 
 class CommentsTransformers extends TransformerAbstract
 {
-    public function transform(Discuss $discussions)
+//    public function transform(Discuss $discussions)
+    public function transform(Comment $comments)
     {
         return [
-            'id'=>$discussions['id'],
-            'title'=>$discussions['title'],
-            'body'=>$discussions['body'],
-            'user_id'=>$discussions['user_id'],
-            'last_user_id'=>$discussions['last_user_id'],
-            'created_at'=>$discussions['created_at'],
-            'comments'=>$discussions->comments
+//            'id'=>$comments->discussion['id'],
+//            'title'=>$comments->discussion['title'],
+//            'body'=>$comments->discussion['body'],
+//            'user_id'=>$discussions['user_id'],
+//            'last_user_id'=>$discussions['last_user_id'],
+//            'created_at'=>$discussions['created_at'],
+            'comment_id'=>$comments->id,
+            'name'=>$comments->user->name,
+            'avatar'=>$comments->user->avatar,
+            'body'=>$comments->body,
+            'created_at'=>$comments->created_at,
         ];
     }
 }
