9add859ca1de9ea0f09043785fbaaa6164f6701e
Robinson28years <1783555833@qq.com>
2017-02-24 13:10:32 +0800

    改标题

--- resources/views/item/acm/sign.blade.php
+++ resources/views/item/acm/sign.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         {{-- <div class="col-md-8 col-md-offset-2"> --}}
             <div class="panel panel-default">
-                <div class="panel-heading">丽水学院ACM程序设计大赛报名</div>
+                <div class="panel-heading">丽水学院第十三届程序设计大赛报名</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="/acm/store">
                         {{ csrf_field() }}
