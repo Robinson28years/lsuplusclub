c873a41e7f55a7379e0fa0cdc394583b449c2927
Robinson28years <1783555833@qq.com>
2017-02-28 18:47:57 +0800

    网页精简

--- resources/views/item/acm/success.blade.php
+++ resources/views/item/acm/success.blade.php
@@ -8,6 +8,8 @@
                 <div class="panel-heading">丽水学院ACM程序设计大赛报名</div>
                 <div class="panel-body">
 					<h2><span class="ion-checkmark-round"></span>恭喜你报名成功</h2>
+                    <p>比赛地点:工科实训大楼机房(教18B406) </p>
+                    <p>比赛时间:3月11日12:30-16:30</p>
                 </div>
             </div>
         {{-- </div> --}}
