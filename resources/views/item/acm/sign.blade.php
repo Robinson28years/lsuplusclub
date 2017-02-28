0ef79359713dc2827b796c3f464bf2925261a4ba
Robinson28years <1783555833@qq.com>
2017-02-28 18:35:46 +0800

    网页精简

--- resources/views/item/acm/sign.blade.php
+++ resources/views/item/acm/sign.blade.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.nonav')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>丽水学院ACM</title>
-@yield('style')
-<!-- Styles -->
-    <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
-    <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
-</head>
-<body>
+@section('content')
 <div class="container">
     <div class="row">
         {{-- <div class="col-md-8 col-md-offset-2"> --}}
@@ -129,5 +107,4 @@
 <div class="container">
         <p>Copyright © 2017 LSU ACM Team </p>
 </div>
-</body>
-</html>
+@endsection
