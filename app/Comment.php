7955efea6e3751b8653d8d5af0e70e8fdcd19887
bb chen <bb.chen@outlook.com>
2017-08-22 16:49:48 +0800

    论坛的topic和comments模型层

---
+++ app/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'comments';
+    protected $fillable = ['id', 'body','user_id','discussion_id'];
+    public function getCreatedAtAttribute($date)
+    {
+        if (Carbon::now() < Carbon::parse($date)->addDays(10)) {
+            return Carbon::parse($date)->diffForHumans();
+        }
+        return Carbon::parse($date);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function discussion()
+    {
+        return $this->belongsTo(Discuss::class,'discussion_id');
+    }
+}
