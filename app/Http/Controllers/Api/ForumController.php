d72d8a4b90a679dc9937fecb2588ee3a5c196427
bb chen <bb.chen@outlook.com>
2017-08-25 10:42:45 +0800

    reformat

--- app/Http/Controllers/Api/ForumController.php
+++ app/Http/Controllers/Api/ForumController.php
@@ -12,6 +12,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class ForumController extends Controller
 {
     public $discussion;
+
     public function __construct()
     {
         $this->discussion = new Forum();
@@ -19,21 +20,23 @@ class ForumController extends Controller
 
     public function index()
     {
-        $discussions = Forum::orderBy('updated_at','desc')->paginate(10);
+        $discussions = Forum::orderBy('updated_at', 'desc')->paginate(10);
         foreach ($discussions as $discuss) {
             $discuss->user = User::find($discuss->user_id);
             $discuss->last_user = User::find($discuss->last_user_id);
         }
         return $discussions;
     }
+
     public function show($id)
     {
         return $this->discussion->show($id);
     }
-    public  function store(Request $request)
+
+    public function store(Request $request)
     {
         $user = JWTAuth::parseToken()->authenticate();
-        $topic = array_merge($request->all(), array("user_id"=>$user->id,"last_user_id"=>0));
+        $topic = array_merge($request->all(), array("user_id" => $user->id, "last_user_id" => 0));
 //        dd($topic);
         $validator = Validator::make($request->all(), [
             'title' => 'required|max:255',
@@ -44,12 +47,13 @@ class ForumController extends Controller
             return $validator->errors();
         }
         $this->discussion->create($topic);
-        return response()->json(["result"=>"success"], 200);
+        return response()->json(["result" => "success"], 200);
     }
+
     public function update(Request $request)
     {
         $user = JWTAuth::parseToken()->authenticate();
-        $topic = array_merge($request->all(), array("user_id"=>$user->id,"last_user_id"=>0));
+        $topic = array_merge($request->all(), array("user_id" => $user->id, "last_user_id" => 0));
 //        dd($topic);
         $validator = Validator::make($request->all(), [
             'id' => 'required',
@@ -60,11 +64,12 @@ class ForumController extends Controller
         if ($validator->fails()) {
             return $validator->errors();
         }
-        if($this->discussion->findOrFail($topic['id'])->user_id != $user->id){
-            return response()->json(["result"=>"无权限进行此操作"], 200);
+        if ($this->discussion->findOrFail($topic['id'])->user_id != $user->id) {
+            return response()->json(["result" => "无权限进行此操作"], 200);
         }
 
         $this->discussion->findOrFail($topic['id'])->update($request->all());
-        return response()->json(["result"=>"success"], 200);
+        return response()->json(["result" => "success"], 200);
     }
+
 }
