f0e83c003d96d310343bf06e94d03dd5ec9181d8
bb chen <bb.chen@outlook.com>
2017-08-30 21:26:30 +0800

    邮件模板

---
+++ resources/views/emails/forget.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>用于重设LSUPlus俱乐部密码的验证码</h1>
+<br>
+<br>
+<p>
+    尊敬的{{$email}}
+</p>
+<p>
+    要重设您的密码，请在看到提示后输入此验证码：{{$code}}
+</p>
+<br>
+<br>
+此致
+<br>
+LSUPlus CLUB
+</body>
+</html>
